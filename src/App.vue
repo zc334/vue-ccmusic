4eaecfe98cc1187200177ed6b4e7b4a5277b5ea6
zc334 <Heine334@163.com>
2018-07-05 14:41:04 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        hello vue
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable";
+    #app 
+        color: $color-theme
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App'
+
+import  'common/stylus/index.styl'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
